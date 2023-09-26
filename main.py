7524474c8206369d249146f0a5cf50a326adf6b2
Jack <132309235+tnp5045@users.noreply.github.com>
2023-09-27 11:47:20 +1300

    456456e

---
+++ main.py
@@ -0,0 +1,9 @@
+def to_upper(name):
+    return name.upper()
+def say_hello(name):
+    print(f'Hello, {name}')
+    if __name__ == '__main__':
+        name = 'Tony'
+        say_hello(name)
+        up = to_upper(name)
+        print(up)
